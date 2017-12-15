1538836097f5c7cc415069ddc183171156a23b5a
Polluux <polluux88@yahoo.fr>
2017-12-15 15:28:46 +0100

    UserEntity with get/set

--- src/main/java/TinyTwitterAPI/UserEntity.java
+++ src/main/java/TinyTwitterAPI/UserEntity.java
@@ -5,4 +5,25 @@ import com.googlecode.objectify.annotation.*;
 public class UserEntity {
 	@Id Long IdUser;
 	String name;
+	
+	public UserEntity(Long id, String n) {
+		IdUser = id;
+		name = n;
+	}
+	
+	public Long getId() {
+		return IdUser;
+	}
+	
+	public void setId(Long id) {
+		IdUser = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
 }
