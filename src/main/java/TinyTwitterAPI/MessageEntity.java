5a841f90ffc60280cb27aac671610bf50dca046f
Florent ALAPETITE <florent037@gmail.com>
2017-12-15 15:51:18 +0100

    API Rest description

--- src/main/java/TinyTwitterAPI/MessageEntity.java
+++ src/main/java/TinyTwitterAPI/MessageEntity.java
@@ -5,9 +5,44 @@ import com.googlecode.objectify.Key;
 
 @Entity
 public class MessageEntity {
-	@Id Long IdMessage;
+	@Id Long idMessage;
 	String message;
-	//Key<UserEntity> driver; 
-
-
+	Key<UserEntity> user;
+	Long timestamp;
+	
+	
+	public Long getIdMessage() {
+		return idMessage;
+	}
+	
+	public void setIdMessage(Long id) {
+		idMessage = id;
+	}
+	
+	
+	public String getMessage() {
+		return message;		
+	}
+	
+	public void setMessage(String m) {
+		message=m;
+	}
+	
+	
+	public Key<UserEntity> getUser(){
+		return user;
+	}
+	
+	public void setUser(Key<UserEntity> k) {
+		user=k;
+	}
+	
+	
+	public void setTimestamp(Long t) {
+		timestamp = t;
+	}
+	
+	public Long getTimestamp() {
+		return timestamp;
+	}
 }
--- src/main/java/TinyTwitterAPI/TinyTwitterREST.java
+++ src/main/java/TinyTwitterAPI/TinyTwitterREST.java
@@ -4,10 +4,27 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
+import com.googlecode.objectify.ObjectifyService;
+
+import javax.inject.Named;
+
 @Api(name = "TinyTwitterEndpoint", 
 	 namespace = @ApiNamespace(ownerDomain = "TinyTwitter.com", 
 	 						   ownerName = "TinyTwitter.com",
 	 						   packagePath = "services"))
 public class TinyTwitterREST {
-
+	
+	@ApiMethod(name = "getMessageById")
+	public MessageEntity getMessageById(@Named("id") Long id) {		
+		MessageEntity message = ObjectifyService.ofy().load().type(MessageEntity.class).filter("idMessage", id).first().now();
+		
+		return message;
+	}
+	
+	
+	@ApiMethod(name = "insertNewMessage")
+	public MessageEntity insertNewMessage(MessageEntity message) {
+		ObjectifyService.ofy().save().entity(message);
+		return message;
+	}
 }
